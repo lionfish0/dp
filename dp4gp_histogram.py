eadb402429c6b8ea61fff123dd2753b63e975d4b
Michael T Smith <lionfishy@gmail.com>
2017-03-10 11:00:29 +0000

    Got histogram DP working and packaged

--- dp4gp_histogram.py
+++ dp4gp_histogram.py
@@ -1,3 +1,4 @@
+
 import dp4gp
 import GPy
 from sklearn.metrics import mean_squared_error
@@ -8,89 +9,80 @@ from scipy.stats import multivariate_normal
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 
-class DPGP_histogram(DPGP):
+def bin_data(Xtest,X,step,ys):
+    """
+    Bin data X into equally sized bins defined by Xtest and step.
+    Xtest is the coordinates of the corner of each bin.
+    step is a vector of step sizes.
+    ys are the outputs (to be summed and averaged)
+    
+    Returns:
+    bincounts
+    bintotals
+    binaverages = bintotals/bincounts
+    """
+    bintotals = np.zeros(Xtest.shape[0])
+    bincounts = np.zeros(Xtest.shape[0])
+    for i,tile in enumerate(Xtest): #loop through the tiles
+        for x,y in zip(X,ys): #loop through the data
+            intile = True
+            for tiled,xd,s in zip(tile,x,step): #loop through the dimensions of the current tile, data and step
+                if (xd<tiled) or (xd>tiled+s):
+                    intile = False
+                    break
+            if intile:
+                bintotals[i]+=y
+                bincounts[i]+=1
+    binaverages = bintotals/bincounts
+    return bincounts, bintotals, binaverages
+
+class DPGP_histogram(dp4gp.DPGP):
     """Using the histogram method"""
     
-    def __init__(self,model,sens,epsilon,delta):      
-        super(DPGP_histogram, self).__init__(model,sens,epsilon,delta)
+    def __init__(self,sens,epsilon,delta):      
+        super(DPGP_histogram, self).__init__(None,sens,epsilon,delta)
 
-    def draw_prediction_samples(self,Xtest,N=1):
-        GPymean, covar = self.model.predict(Xtest)
-        mean, noise, cov = self.draw_noise_samples(Xtest,N,Nattempts,Nits)
-        #TODO: In the long run, remove DP4GP's prediction code and just use GPy's
-        #print GPymean-mean
-        assert np.max(GPymean-mean)<1e-3, "DP4GP code's posterior mean prediction differs from GPy's"
-        return mean + noise.T, mean, cov
-        
-    def draw_noise_samples(self,Xtest,N=1,Nattempts=7,Nits=1000):
+    def prepare_model(self,Xtest,X,step,ys):
         """
-        Provide N samples of the DP noise
-        """
-        sigmasqr = self.model.Gaussian_noise.variance[0]
-        K_NN = self.model.kern.K(self.model.X)
-        K_NNinv = np.linalg.inv(K_NN+sigmasqr*np.eye(K_NN.shape[0]))
-        K_Nstar = self.model.kern.K(Xtest,self.model.X)
-        C = np.dot(K_Nstar,K_NNinv)
+        Prepare the model, ready for making predictions"""
+        bincounts, bintotals, binaverages = bin_data(Xtest,X,step,ys)
+        sens_per_bin = self.sens/bincounts
+        c = np.sqrt(2*np.log(2/self.delta)) #1.25 or 2 over delta?
+        bin_sigma = c*sens_per_bin/self.epsilon #noise standard deviation to add to each bin
+        #add DP noise to the binaverages
+        dp_binaverages=binaverages+np.random.randn(binaverages.shape[0])*bin_sigma
 
-        print C.shape
-        cs = []
-        for i in range(C.shape[1]):
-            cs.append(C[:,i][:,None])
+        #we need to build the input for the integral kernel
+        newXtest = np.zeros([Xtest.shape[0],2*Xtest.shape[1]])
+        newXtest[:,0::2] = Xtest+step
+        newXtest[:,1::2] = Xtest
+
+        #we don't want outputs that have no training data in.
+        keep = ~np.isnan(dp_binaverages)
+        finalXtest = newXtest[keep,:]
+        final_dp_binaverages = dp_binaverages[keep]
+
         
-        ls = self.findLambdas_repeat(cs,Nattempts,Nits)
-        M = self.calcM(ls,cs)
+        #the integral kernel takes as y the integral... 
+        #eg. if there's one dimension we're integrating over, km
+        #then we need to give y in pound.km
+        self.meanoffset = np.mean(final_dp_binaverages)
+        final_dp_binaverages-= self.meanoffset
+        finalintegralbinaverages = final_dp_binaverages * np.prod(step) 
+        final_sigma = bin_sigma[keep]
+        finalintegralsigma = final_sigma * np.prod(step)
         
-        c = np.sqrt(2*np.log(2/self.delta))
-        Delta = self.calcDelta(ls,cs)
-        #in Hall13 the constant below is multiplied by the samples,
-        #here we scale the covariance by the square of this constant.
-        sampcov = ((self.sens*c*Delta/self.epsilon)**2)*M
-        samps = np.random.multivariate_normal(np.zeros(len(sampcov)),sampcov,N)
-        
-        ###This code is only necessary for finding the mean
-        mu = np.dot(C,self.model.Y)
-        ###
-        return mu, samps, sampcov
+        #generate the integral model
+        kernel = GPy.kern.Multidimensional_Integral_Limits(input_dim=newXtest.shape[1], variances=1.0, lengthscale=[3e3,3e3])
+        #we add a kernel to describe the DP noise added
+        kernel = kernel + GPy.kern.WhiteHeteroscedastic(input_dim=newXtest.shape[1], num_data=len(finalintegralsigma), variance=finalintegralsigma**2)
+        self.model = GPy.models.GPRegression(finalXtest,finalintegralbinaverages[:,None],kernel)
+        self.model.optimize() #TODO remove max_iters
     
-    def plot(self,fixed_inputs=[],legend=False,plot_data=False, steps=10, N=10, Nattempts=1, Nits=500):
-        """
-        Plot the DP predictions, etc.
         
-        In 2d it shows one DP sample, the size of the circles represent the prediction values
-        the alpha how much DP noise has been added (1->no noise, 0->20% of max-min prediction
-        
-        fixed_inputs = list of pairs
-        legend = whether to plot the legend
-        plot_data = whether to plot data
-        steps = resolution of plot
-        N = number of DP samples to plot (in 1d)
-        Nattempts = number of times a DP solution will be looked for (can help avoid local minima)
-        Nits = number of iterations when finding DP solution
-        (these last two parameters are passed to the draw_prediction_samples method).
-        """
-        Xtest, free_inputs = compute_Xtest(self.model.X, fixed_inputs, extent_lower={}, steps=steps)
-        preds, mu, cov = self.draw_prediction_samples(Xtest,N,Nattempts=1,Nits=Nits)
-        self.model.plot(fixed_inputs=fixed_inputs,legend=legend,plot_data=False)
-        DPnoise = np.sqrt(np.diag(cov))
-        indx = 0
-        if len(free_inputs)==2:
-            minpred = np.min(mu)
-            maxpred = np.max(mu)
-            scaledpreds = 1+1000*(preds[:,indx]-minpred) / (maxpred-minpred)
-            scalednoise = 1-5*DPnoise/(maxpred-minpred) #proportion of data
-            #any shade implies the noise is less than 20% of the total change in the signal
-            scalednoise[scalednoise<0] = 0
-            rgba = np.zeros([len(scalednoise),4])
-            rgba[:,0] = 1.0
-            rgba[:,3] = scalednoise
-            plt.scatter(Xtest[:,free_inputs[0]],Xtest[:,free_inputs[1]],scaledpreds,color=rgba)
-            plt.scatter(Xtest[:,free_inputs[0]],Xtest[:,free_inputs[1]],scaledpreds,facecolors='none')
-            if plot_data: #do this bit ourselves
-                plt.plot(self.model.X[:,free_inputs[0]],self.model.X[:,free_inputs[1]],'.k',alpha=0.2)
-
-
-        if len(free_inputs)==1:
-            plt.plot(Xtest[:,free_inputs[0]],preds,alpha=0.2,color='black')
-            plt.plot(Xtest[:,free_inputs[0]],mu[:,0]-DPnoise,'--k',lw=2)
-            plt.plot(Xtest[:,free_inputs[0]],mu[:,0]+DPnoise,'--k',lw=2)
-
+    def draw_prediction_sample(self,Xtest):
+        newXtest = np.zeros([Xtest.shape[0],2*Xtest.shape[1]])
+        newXtest[:,0::2] = Xtest
+        newXtest[:,1::2] = 0
+        mean, cov = self.model.predict(newXtest)
+        return mean+self.meanoffset, cov
